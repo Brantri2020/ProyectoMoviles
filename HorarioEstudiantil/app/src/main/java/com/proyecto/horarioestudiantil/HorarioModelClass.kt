8cb5389f60bd3830767d098ff199400f0d9e033e
RENE SIMBAÑA <jairo-rene@hotmail.com>
2021-03-16 00:02:16 -0500

    Modelo horario

--- HorarioEstudiantil/app/src/main/java/com/proyecto/horarioestudiantil/HorarioModelClass.kt
+++ HorarioEstudiantil/app/src/main/java/com/proyecto/horarioestudiantil/HorarioModelClass.kt
@@ -1,3 +1,7 @@
 package com.proyecto.horarioestudiantil
 
-data class HorarioModelClass(var idSchedule: Int,var idSubject: Int, var dia:String, var horaInicio:String, var horaFin:String)
+data class HorarioModelClass(var materia:String,var dia:String, var horaInicio:String, var horaFin:String) {
+
+    constructor():this("","","","")
+
+}
