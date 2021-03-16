a7bae75556bca69de1809ee4236d0a004f132d6d
juanfran96 <59719815+juanfran96@users.noreply.github.com>
2021-03-15 20:41:14 -0500

    Cambio materiaAdapter

--- HorarioEstudiantil/app/src/main/java/com/proyecto/horarioestudiantil/MateriaAdapter.kt
+++ HorarioEstudiantil/app/src/main/java/com/proyecto/horarioestudiantil/MateriaAdapter.kt
@@ -6,17 +6,18 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
 
-class MateriaAdapter (private val context: Activity, private val materias: ArrayList<MateriaModelClass>)   : BaseAdapter() {
+class MateriaAdapter (private val context: Activity, private val materias: ArrayList<String>)   : BaseAdapter() {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val inflater = context.layoutInflater
         val rowView = inflater.inflate(R.layout.subject_list, null, true)
 
+        //val materias = mutableListOf<String>()
 
 
         val textViewSubject = rowView.findViewById<TextView>(R.id.textViewSubject)
 
 
-        textViewSubject.text = "  ${materias[position].NameSubject}   "
+        textViewSubject.text = "  ${materias[position]}   "
         return rowView
     }
 
