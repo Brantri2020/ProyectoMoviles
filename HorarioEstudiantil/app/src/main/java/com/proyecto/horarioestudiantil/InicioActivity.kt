997701d00eb0b7780d926f87de51aae548bb1676
juanfran96 <59719815+juanfran96@users.noreply.github.com>
2021-03-15 20:43:06 -0500

    Cambio Inicio

--- HorarioEstudiantil/app/src/main/java/com/proyecto/horarioestudiantil/InicioActivity.kt
+++ HorarioEstudiantil/app/src/main/java/com/proyecto/horarioestudiantil/InicioActivity.kt
@@ -2,36 +2,42 @@ package com.proyecto.horarioestudiantil
 
 import android.content.Intent
 import android.content.pm.ActivityInfo
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class InicioActivity : AppCompatActivity() {
+
+    var estudiantes = arrayListOf<EstudianteModelClass>()
+    var estudiantesIdDocumentos = ArrayList<String>()
+    // Access a Cloud Firestore instance from your Activity
+    val db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
         ///Cambiar a modo vertical
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
 
-        //Id del estudiante logueado
-        var idEstudiante:String
-        var nombreEstudiante:String
-        idEstudiante = intent.getStringExtra(ID_ESTUDIANTE).toString()
-        nombreEstudiante = intent.getStringExtra(NOMBRE_ESTUDIANTE).toString()
+        //Correo del estudiante logueado
+        var correoEstudiante: String
+        correoEstudiante = intent.getStringExtra(CORREO_ESTUDIANTE).toString()
 
-        val actionBar = supportActionBar
-        // Set the action bar title, subtitle and elevation
-        actionBar!!.title = supportActionBar?.title.toString()
-        actionBar.subtitle = nombreEstudiante
-        actionBar.elevation = 4.0F
+        nombresEstudiante(correoEstudiante)
+
 
 
 
         val buttonRegistrarMateria = findViewById(R.id.btnRegistrarMateria) as Button
         buttonRegistrarMateria.setOnClickListener {
             val n = Intent(this, RegistrarMateriaActivity::class.java)
-            n.putExtra(ID_ESTUDIANTE,idEstudiante.toString())
+            n.putExtra(CORREO_ESTUDIANTE, correoEstudiante)
             startActivity(n)
 
         }
@@ -41,7 +47,7 @@ class InicioActivity : AppCompatActivity() {
         val buttonRegistrarHorario = findViewById(R.id.btnRegistrarHorario) as Button
         buttonRegistrarHorario.setOnClickListener {
             val i = Intent(this, RegistrarHorarioActivity::class.java)
-            i.putExtra(ID_ESTUDIANTE,idEstudiante.toString())
+           i.putExtra(CORREO_ESTUDIANTE,correoEstudiante)
             startActivity(i)
 
         }
@@ -56,20 +62,55 @@ class InicioActivity : AppCompatActivity() {
         val buttonRegistrarRecordatorio = findViewById(R.id.btnRegistrarRecordatorio) as Button
         buttonRegistrarRecordatorio.setOnClickListener {
             val j = Intent(this, RegistrarRecordatorioActivity::class.java)
+            j.putExtra(CORREO_ESTUDIANTE,correoEstudiante)
             startActivity(j)
         }
 
-        val buttonVerRecordatorio = findViewById(R.id.btnVerRecordatorio) as Button
-        buttonVerRecordatorio.setOnClickListener {
-            val k = Intent(this, VerRecordatoriosActivity::class.java)
-            startActivity(k)
-        }
 
         val buttonSalirInicio = findViewById(R.id.btnSalir) as Button
         buttonSalirInicio.setOnClickListener {
             val l = Intent(this, LoginActivity::class.java)
+            FirebaseAuth.getInstance().signOut();
             startActivity(l)
         }
 
     }
+
+
+fun nombresEstudiante(correo:String){
+
+    var nombres:String=""
+    var nombre:String=""
+    var apellido:String=""
+    val docRef = db.collection(COLECCION).document(correo)
+
+    docRef.get()
+        .addOnSuccessListener { document ->
+            if (document != null) {
+
+                nombre=document.get("nombre").toString()
+                apellido=document.get("apellido").toString()
+
+                nombres=nombre+" "+apellido
+
+                val actionBar = supportActionBar
+                // Set the action bar title, subtitle and elevation
+                actionBar!!.title = supportActionBar?.title.toString()
+                actionBar.subtitle = nombres
+                actionBar.elevation = 4.0F
+
+
+            } else {
+                // Log.d(TAG, "No such document")
+            }
+        }
+        .addOnFailureListener { exception ->
+            //Log.d(TAG, "get failed with ", exception)
+        }
+
+
+}
+
+
+
 }
